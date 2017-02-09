0e049d58ef4b461135fdbfe4183c3623eb58f928
oliver.buckler@gmail.com <oliver.buckler@gmail.com>
2017-02-09 21:51:06 +0800

    Add project files.

---
+++ Monitor/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(12, 12);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(437, 342);
+            this.lbLog.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(374, 360);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(293, 360);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "STATE:";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(63, 365);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(27, 13);
+            this.lblState.TabIndex = 4;
+            this.lblState.Text = "N/A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 389);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "HB ProcMon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblState;
+    }
+}
+
---
+++ Monitor/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Monitor
+{
+    public partial class Form1 : Form
+    {
+        List<Process> ProcessList = new List<Process>();
+        Process HonorBuddyClient;
+        const string PROC_NAME = @"Honorbuddy";
+
+        BindingList<string> Log = new BindingList<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitializeLog();
+            GetHonorBuddyClient();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeLog()
+        {
+            Log.Clear();
+            lbLog.DataSource = Log;
+        }
+
+        private void GetHonorBuddyClient()
+        {
+            ProcessList = Process.GetProcesses().ToList();
+
+            HonorBuddyClient = ProcessList.FirstOrDefault(x => x.ProcessName.Contains(PROC_NAME));
+
+            if (HonorBuddyClient == null)
+            {
+                Log.Add("Honorbuddy client not running.");
+            }
+            else
+            {
+                Log.Add("Honorbuddy client found.");
+            }
+
+        }
+    }
+}
